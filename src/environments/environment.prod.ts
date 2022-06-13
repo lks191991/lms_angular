634014dd389289a47a7ce01a3994e88eaef11d8e
dslokesh <lokesh.kumar@dotsquares.com>
2022-06-13 10:51:55 +0530

    init file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  url: 'https://civil.learnersquare.com/api/',
+  base_api: 'https://civil.learnersquare.com/api/',
+  base_url: 'https://civil.learnersquare.com/',
+  stripeKey: 'pk_test_FectS22rEqhkAWBVCkTOSsf1006GpN51sk',
+  stripeSecret: 'sk_test_lXHPsq4lMluzlkHzatrSOqpe00PC47Q8SU',
+  collegeName : 'Online Course'
+};
